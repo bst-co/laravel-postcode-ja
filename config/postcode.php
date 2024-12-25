45cc4bc14d75f77956981ac587a8729f78574e49
t-ueno <t-ueno@bstinc.co.jp>
2024-12-25 15:14:42 +0900

    Standardize postcode config and add support for multiple countries.
    Updated the postcode configuration to support multiple countries with distinct formats, including JP, US, CA, GB, CN, and SG. Standardized keys to lowercase country codes and added new configuration options for database, sources, and formatting details. These changes enhance flexibility and ensure proper postcode processing for diverse regions.

--- config/postcode.php
+++ config/postcode.php
@@ -4,17 +4,72 @@ use BstCo\PostcodeJa\Services\PostcodeParse\JPNParser;
 
 return [
     'country' => [
-        'default' => 'JPN',
+        'default' => 'jp',
+    ],
+
+    'database' => env('POSTCODE_DATABASE'),
+
+    'sources' => [
+        // Japan
+        'jp' => [
+            // DataSource URL
+            'source' => 'https://www.post.japanpost.jp/zipcode/dl/utf/zip/utf_ken_all.zip',
+            // Expanded file name
+            'expand' => 'utf_ken_all.csv',
+            // Post code length
+            'length' => 7,
+            // Post code padding string
+            'padding' => '0',
+            // Post code separating format
+            'format' => [3, 4],
+            // Post code separator
+            'separator' => '-',
+        ],
+        // United of State
+        'us' => [
+            'length' => [5, 9],
+            'padding' => '0',
+            'format' => [5, 4],
+            'separator' => '-',
+        ],
+        // Canada
+        'ca' => [
+            'length' => 6,
+            'padding' => '0',
+            'format' => [3, 3],
+            'separator' => ' ',
+        ],
+        // United Kingdom of Great Britain and Northern Ireland
+        'gb' => [
+            'length' => 6,
+            'padding' => '0',
+            'format' => [3, 3],
+            'separator' => ' ',
+        ],
+        // People's Republic of China
+        'cn' => [
+            'length' => 6,
+            'padding' => '0',
+            'format' => [6],
+            'separator' => '',
+        ],
+        // Republic of Singapore
+        'sg' => [
+            'length' => 6,
+            'padding' => '0',
+            'format' => [6],
+            'separator' => '',
+        ]
     ],
 
     'source' => [
-        'JPN' => [
+        'jp' => [
             'url' => 'https://www.post.japanpost.jp/zipcode/dl/utf/zip/utf_ken_all.zip',
             'file' => 'utf_ken_all.csv',
         ],
     ],
 
     'parsers' => [
-        'JPN' => JPNParser::class,
-    ]
+        'jp' => JPNParser::class,
+    ],
 ];
